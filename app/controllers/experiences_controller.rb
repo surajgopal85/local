8e0d66aee8c18bb25c99135689de5f4f6bb09a15
Suraj Gopal <surajcancode@gmail.com>
2022-11-24 13:38:13 -0500

    add experience w multiple experience tags works, added cocoon gem, cocoon js to app js file, nested form for adding mult e_ts to exp, a few route changes, yarn also updated --- do not forget to bundle & yarn for this to work

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,15 +3,38 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
   end
 
+  def new
+    @experience = Experience.new
+    @tags = Tag.all
+    @experience.experience_tags.build unless @experience.experience_tags.any?
+  end
+
+  def create
+    @tags = Tag.all
+    @experience = Experience.new(experience_params)
+    @experience.local = current_user
+
+    if @experience.save
+      redirect_to local_experiences_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @experience = Experience.find(params[:id])
   end
 
-  def my_experiences
-
+  def local_experiences
+    @user = current_user
+    @experiences = Experience.where(local_id: current_user.id)
   end
 
 
+  private
 
+  def experience_params
+    params.require(:experience).permit(:name, :location, :description, :image_url, experience_tags_attributes: [:id, :tag_id, :_destroy])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :experiences, only: %i[index show]
+  resources :experiences, only: %i[index show new create]
   get '/my_experiences', to: 'experiences#my_experiences', as: :my_experiences
+  get '/local_experiences', to: 'experiences#local_experiences', as: :local_experiences
   get '/my_experiences/:id', to: 'experiences#my_experience', as: :my_experience
 
   resources :trips
