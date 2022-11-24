10f83db2485971bdb67599b0374fa8a80aaa778f
Alex Trimper <trimper.a@gmail.com>
2022-11-24 16:06:49 -0400

    added links to developer navbar and fixed missing end in experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -25,7 +25,6 @@ class ExperiencesController < ApplicationController
     @experience = Experience.find(params[:id])
   end
 
-
   def my_experiences
     @trip = Trip.find(params[:trip_id])
     @experiences = Experience.where("location ILIKE ?", "%" + @trip.city + "%")
@@ -35,18 +34,16 @@ class ExperiencesController < ApplicationController
     @trip = Trip.find(params[:trip_id])
     @experience = Experience.find(params[:id])
     # @trip_experience = TripExperience.new
+  end
 
   def local_experiences
     @user = current_user
     @experiences = Experience.where(local_id: current_user.id)
   end
 
-
   private
 
   def experience_params
     params.require(:experience).permit(:name, :location, :description, :image_url, experience_tags_attributes: [:id, :tag_id, :_destroy])
-
   end
-
 end
