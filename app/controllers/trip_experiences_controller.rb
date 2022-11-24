1c56a888c0ecbceaa936be036256aca7e0da290c
Helene Buffet <hebuffet@gmail.com>
2022-11-24 13:29:24 -0500

    added some validation and links to pages

--- app/controllers/trip_experiences_controller.rb
+++ app/controllers/trip_experiences_controller.rb
@@ -2,13 +2,14 @@ class TripExperiencesController < ApplicationController
   def create
     @trip = Trip.find(params[:trip_id])
     @experience = Experience.find(params[:id])
+
     @trip_experience = TripExperience.new()
     @trip_experience.trip = @trip
     @trip_experience.experience = @experience
     if @trip_experience.save
       redirect_to trip_path(@trip)
     else
-      render 'trips/show', status: unprocessable_entity
+      render 'trips/show', status: :unprocessable_entity
     end
   end
 end
--- app/models/trip_experience.rb
+++ app/models/trip_experience.rb
@@ -1,4 +1,6 @@
 class TripExperience < ApplicationRecord
   belongs_to :experience
   belongs_to :trip
+
+  validates :experience_id, uniqueness: { scope: :trip_id }
 end
